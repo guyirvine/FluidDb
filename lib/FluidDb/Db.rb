4763ad11dfe3e6c19e8236ee48f61ec5b69cdb60
Guy Irvine <guy@guyirvine.com>
2012-11-06 16:30:03 +1300

    Added Factory

---
+++ lib/FluidDb/Db.rb
@@ -0,0 +1,25 @@
+require "FluidDb"
+
+module FluidDb
+    
+    def FluidDb.Db( uri )
+        
+        case uri.scheme
+            when "mysql"
+            require "FluidDb/Mysql"
+            return FluidDb::Mysql.new( uri )
+            when "mysql2"
+            require "FluidDb/Mysql2"
+            return FluidDb::Mysql2.new( uri )
+            when "pgsql"
+            require "FluidDb/Pgsql"
+            return FluidDb::Pgsql.new( uri )
+            when "mock"
+            require "FluidDb/Mock"
+            return FluidDb::Mock.new( uri )
+            else
+            abort("Scheme, #{uri.scheme}, not recognised when configuring creating db connection");
+        end
+        
+    end
+end
